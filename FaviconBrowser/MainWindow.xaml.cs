4d76fa1979ddfa377f42013fe4c7ffc4a11a4274
alexdavies74 <devnull@localhost>
2012-05-07 11:17:38 +0100

    Basic getting of favicons

--- FaviconBrowser/MainWindow.xaml.cs
+++ FaviconBrowser/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,26 @@ namespace FaviconBrowser
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddAFavicon("google.com");
+        }
+
+        private void AddAFavicon(string domain)
+        {
+            WebClient webClient = new WebClient();
+            byte[] bytes = webClient.DownloadData("http://" + domain + "/favicon.ico");
+            Image imageControl = new Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = new MemoryStream(bytes);
+            bitmapImage.EndInit();
+            imageControl.Source = bitmapImage;
+            imageControl.Width = 16;
+            imageControl.Height = 16;
+            m_WrapPanel.Children.Add(imageControl);
         }
     }
 }
